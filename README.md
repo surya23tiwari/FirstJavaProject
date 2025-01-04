62b39f3237710bae7af2ac6e60ce2d3a1c3e7d8b
Surya Tiwari <surya23tiwari@gmail.com>
2025-01-04 20:15:29 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# FirstJavaProject
+# FirstJavaProject
---
+++ helloWord.java
@@ -0,0 +1,8 @@
+helloWord.java
+class Test
+{
+    public static void main(String []args)
+    {
+        System.out.println("My First Java Program.");
+    }
+};
